a6dbc146b6b0bf4e23aa0482908a647b1b04b203
Runningman1234 <cbrunerstu@gmail.com>
2016-10-02 23:08:02 -0400

    Add files via upload
    Fix build process

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    devtool: "eval",
+    entry: [
+      'webpack-dev-server/client?http://0.0.0.0:8010',
+      'webpack/hot/only-dev-server',
+      './index.jsx',
+    ],
+    output: {
+        filename: 'bundle.js',
+        publicPath: 'http://localhost:8010/assets',
+    },
+    module: {
+        loaders: [
+            {
+                // tell webpack to use babel for all *.jsx and *.js files
+                test: /\.jsx?$/,
+                loaders: ['react-hot', 'babel-loader'],
+                exclude: /\/node_modules\/|\/lib\//
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+};
