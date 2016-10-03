9eebcf5d0947c7f678ea86e4ff49d8cc09df5856
Runningman1234 <cbrunerstu@gmail.com>
2016-10-02 23:14:01 -0400

    Add files via upload
    merch page

---
+++ index.jsx
@@ -0,0 +1,23 @@
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+const { Router, Route, Link, browserHistory } = require('react-router')
+
+const production = process.env.NODE_ENV === "production";
+
+const HomePage = require('./components/homepage.jsx');
+const About = require('./components/about.jsx');
+const Shows = require('./components/shows.jsx');
+const Youtube = require('./components/youtube.jsx');
+const Merch = require('./components/merch.jsx');
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route name="home" path="/" component={HomePage}/>
+    <Route name="about" path="/about" component={About}/>
+    <Route name="shows" path="/shows" component={Shows}/>
+    <Route name="youtube" path="/youtube" component={Youtube}/>
+    <Route name="merch" path="/merch" component={Merch}/>
+    <Route name="default" path="*" component={HomePage}/>
+  </Router>
+), document.getElementById("content"))
